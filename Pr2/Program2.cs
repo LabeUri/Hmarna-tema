3b1bb8487ec265d1503706a2e53d4bd6c132fd77
Aleksandr <116078256+LabeUri@users.noreply.github.com>
2023-09-27 12:28:54 +0300

    Add files via upload

---
+++ Pr2/Program2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = new int[3, 3];
+
+        // Зчитуємо значення елементів матриці від користувача
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"Введіть елемент [{i + 1},{j + 1}]: ");
+                if (int.TryParse(Console.ReadLine(), out int element))
+                {
+                    matrix[i, j] = element;
+                }
+                else
+                {
+                    Console.WriteLine("Невірний формат числа. Будь ласка, введіть ціле число.");
+                    j--; // Повторити введення для поточного елемента
+                }
+            }
+        }
+
+        int sum = CalculateDiagonalSum(matrix);
+
+        Console.WriteLine($"Сума елементів головної діагоналі матриці: {sum}");
+        Console.WriteLine("Натисніть будь-яку клавішу, щоб завершити...");
+        Console.ReadKey();
+
+    }
+
+    static int CalculateDiagonalSum(int[,] matrix)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            sum += matrix[i, i];
+        }
+
+        return sum;
+    }
+}
